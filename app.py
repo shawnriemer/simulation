e5c9bd3178f944785bd80d13172a3cc110cfa3ca
Shawn Riemer <riemershawn@gmail.com>
2024-09-14 21:49:55 -0500

    changes bruh

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     fig_html = blackjack_sim()
-    # fig_html2 = make_different_plot()
-    return render_template('index.html', graph=fig_html)#, graph2=fig_html2)
+    return render_template('index.html', graph=fig_html)
 
-
+@app.route('/sim', methods=['GET', 'POST'])
+def sim():
+    graph_none = roulette_none()
+    graph_martingale = roulette_martingale()
+    return render_template(
+        'sim.html',
+        graph_none=graph_none,
+        graph_martingale=graph_martingale
+    )
 
 if __name__ == '__main__':  
    app.run(host="0.0.0.0", debug=True)
--- modules/graphs.py
+++ modules/graphs.py
@@ -4,6 +4,8 @@ import pandas as pd
 import plotly.express as px
 import random
 
+def update_fig(fig):
+    pass
 
 def blackjack_sim():
     if request.method == 'POST':
@@ -18,12 +20,11 @@ def blackjack_sim():
     win_prob = .38
     bjack_prob = .04
     draw_prob = .08
-    rounds = n
     lose_prob = 1 - win_prob - bjack_prob - draw_prob
     cashflow_list = []
 
     for i in range(n):
-        cash = 100
+        cash = cash
         cashflow = [cash]
         while cash >= bet:
             cash += random.choices([bet, 1.5 * bet, 0, -1 * bet], weights=[win_prob, bjack_prob, draw_prob, lose_prob])[
@@ -43,8 +44,8 @@ def blackjack_sim():
         dfi,
         x='index',
         y=y,
-        height=1100,
-        width=1100  # 983 makes my phone have scrollbar
+        # height=1100,
+        # width=500  # 983 makes my phone have scrollbar
     )
     fig = fig.update_traces(
         line_color='#ff0000',
@@ -53,7 +54,6 @@ def blackjack_sim():
     fig = fig.update_layout(
         xaxis_fixedrange=True,
         yaxis_fixedrange=True,
-        yaxis_scaleanchor='x',
         xaxis_griddash='dash',
         yaxis_griddash='dash',
         margin=dict(l=0, r=0, t=100, b=0),
@@ -152,3 +152,138 @@ def make_different_plot():
     )
     fig_html = fig.to_html(full_html=False, config={'displayModeBar': False})
     return fig_html
+
+
+def roulette_none():
+    n = 1
+    bet = 20
+    cash = 100
+    win_prob = .474
+    lose_prob = 1 - win_prob
+    cashflow_list = []
+
+    for i in range(n):
+        cash = 100
+        cashflow = [cash]
+        while cash >= bet:
+            cash += random.choices([bet, -1 * bet], weights=[win_prob, lose_prob])[
+                0]
+            cashflow.append(cash)
+        cashflow_list.append(cashflow)
+
+    dfi = pd.DataFrame(cashflow_list)
+    dfi = dfi.transpose().reset_index()
+
+    print(len(cashflow_list[0]) - 1)
+    tickmark_list = create_tickmarks(len(cashflow_list[0]) - 1)
+    print(tickmark_list)
+
+    y = [int(x) for x in np.arange(0, n)]
+    fig = px.line(
+        dfi,
+        x='index',
+        y=y,
+    )
+    fig = fig.update_traces(
+        line_color='#ff0000',
+        line_width=6
+    )
+    fig = fig.update_layout(
+        xaxis_fixedrange=True,
+        yaxis_fixedrange=True,
+        xaxis_griddash='dash',
+        yaxis_griddash='dash',
+        margin=dict(l=0, r=0, t=100, b=0),
+        xaxis_tickfont_size=50,
+        yaxis_tickfont_size=50,
+        xaxis_range=[0, tickmark_list[-1] + 1],
+        xaxis_type='category',
+        yaxis_rangemode='tozero',
+        xaxis_title='Hands',
+        xaxis_title_font_size=50,
+        yaxis_title=None,
+        xaxis_zeroline=True,
+        yaxis_zeroline=False,
+        yaxis_tickprefix='$ ',
+        yaxis_ticksuffix=' ',
+        xaxis_tickvals=tickmark_list,
+        plot_bgcolor='rgb(0, 0, 0, 0)',
+        paper_bgcolor='rgb(0, 0, 0, 0)',
+        showlegend=False,
+        title=f'Roulette - ${bet} Bets',
+        title_x=.5,
+        title_font_size=50,
+        font_color='white',
+        font_family='Arial Black'
+    )
+    fig_html = fig.to_html(full_html=False, config={'displayModeBar': False})
+    return fig_html
+
+def roulette_martingale():
+    n = 1
+    start_cash = 100
+    start_bet = 5
+    win_prob = .47
+    lose_prob = 1 - win_prob
+    cashflow_list = []
+
+    for i in range(n):
+        cash = start_cash
+        bet = start_bet
+        cashflow = [cash]
+        while cash - bet > 0:
+            change = random.choices([bet, -1 * bet], weights=[win_prob, lose_prob])[0]
+            cash += change
+            if change < 0:
+                bet *= 2
+            else:
+                bet = start_bet
+            cashflow.append(cash)
+        t = [x + 1 for x in range(len(cashflow))]
+        cashflow_list.append(cashflow)
+
+    dfi = pd.DataFrame(cashflow_list)
+    dfi = dfi.transpose().reset_index()
+
+    y = [int(x) for x in np.arange(0, n)]
+    fig = px.line(
+        dfi,
+        x='index',
+        y=y
+    )
+    fig = fig.update_traces(
+        line_color='#ff0000',
+        line_width=6
+    )
+    fig = fig.update_layout(
+        xaxis_fixedrange=True,
+        yaxis_fixedrange=True,
+        xaxis_griddash='dash',
+        yaxis_griddash='dash',
+        margin=dict(l=0, r=0, t=100, b=0),
+        xaxis_tickfont_size=50,
+        yaxis_tickfont_size=50,
+        # xaxis_range=[0, tickmark_list[-1]+1],
+        xaxis_type='category',
+        yaxis_rangemode='tozero',
+        xaxis_title='Hands',
+        xaxis_title_font_size=50,
+        yaxis_title=None,
+        xaxis_zeroline=True,
+        yaxis_zeroline=False,
+        yaxis_tickprefix='$ ',
+        yaxis_ticksuffix=' ',
+        # xaxis_tickvals=tickmark_list,
+        plot_bgcolor='rgb(0, 0, 0, 0)',
+        paper_bgcolor='rgb(0, 0, 0, 0)',
+        showlegend=False,
+        title=f'Roulette - ${start_bet} Unit',
+        title_x=.5,
+        title_font_size=50,
+        font_color='white',
+        font_family='Arial Black'
+    )
+    fig_html = fig.to_html(full_html=False, config={'displayModeBar': False})
+    return fig_html
+
+
